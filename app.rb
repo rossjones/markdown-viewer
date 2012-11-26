384e1003c1ea5c758d2e4df40f854dd8349840ca
Ross Jones <rossdjones@gmail.com>
2012-11-26 18:38:05 +0000

    Moved HERE doc to a template

--- app.rb
+++ app.rb
@@ -7,21 +7,7 @@ def render_md(params, forced=nil)
 end
 
 get '/' do
-      @mdown = <<HERE
-# Using Markdown Viewer
-
-Paste your Markdown into this text area, and then click *Show me HTML!* to see what the rendered output looks like.  If you want the HTML source then click *Give me HTML!*
-
-## Using curl
-
-If you want to use curl to convert your Markdown you can POST to http://markdownviewer.herokuapp.com/render passing your Markdown in a variable called _content_.
-
-> curl --data "content=#This is a title" http://markdownviewer.herokuapp.com/render
-
-To send a file (with your Markdown in) use the following, replacing filename.txt with the name of your file
-
-> curl --form "file=@filename.txt" http://markdownviewer.herokuapp.com/render
-HERE
+  @mdown = erb :default_content,:layout=>false
   @result, _ = render_md params, @mdown
   erb :index
 end
