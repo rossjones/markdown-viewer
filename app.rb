b45265aeef0f550b45fc20a6071bc44f5b11e27a
Ross Jones <rossdjones@gmail.com>
2012-11-26 13:05:34 +0000

    Updated the code to successfully render markdown whether you are in browser or not

--- app.rb
+++ app.rb
@@ -1,9 +1,36 @@
 require 'sinatra'
+require 'rdiscount'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
   @x = 'erb'
   erb :index
+end
+
+
+
+post '/render' do
+
+  if params[:content]
+    content = params[:content]
+  else
+    content = params['file'][:tempfile].read
+  end
+
+  @mdown = content
+  @markdown = RDiscount.new(@mdown)
+  @result = @markdown.to_html
+
+  if params[:encode]
+    @encoded = true
+  end
+
+  if params[:site]
+    erb :index
+  else
+    content_type 'text/plain', :charset => 'utf-8'
+    @result
+  end
 end
 
